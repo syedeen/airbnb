28aba73b0fc3dc17c912ac76d5509a0a56a5ed01
syedeen <160634973+syedeen@users.noreply.github.com>
2025-05-11 08:12:38 +0530

    app.js

--- app.js
+++ app.js
@@ -151,6 +151,7 @@ app.use((req,res,next)=>{
 
 app.use((err,req,res,next)=>{
     let{status=500,message}=err;
+    console.log(err);
     res.status(status).render("listings/error.ejs" , {err}) ;
 })
 
